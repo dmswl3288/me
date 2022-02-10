1542a54bf45b98ee707ac39f5933d68222148cbf
HanEunJi <dmswl3288@naver.com>
2022-02-10 12:08:21 +0900

    work subImage 추가.

--- src/components/common/DetailWork.js
+++ src/components/common/DetailWork.js
@@ -12,19 +12,62 @@ import {
     Link,
     Dialog,
     DialogTitle,
+    MobileStepper,
 } from '@mui/material'
-import Fade from 'react-reveal/Fade'
+import CloseIcon from '@mui/icons-material/Close'
+import SwipeableViews from 'react-swipeable-views'
 
 const DetailWork = ({
     handleClose,
     open,
-    title,
+    work,
+    activeStep,
+    setActiveStep,
 }) => {
 
+    const handleStepChange = (step) => {
+        setActiveStep(step)
+    }
+
+    if (!work) return null
+
+    const maxSteps = work.subImgSrc.length
     return (
         <Dialog onClose={handleClose} open={open} fullWidth={true}>
-            <DialogTitle>{title}</DialogTitle>
-            
+            <DialogTitle
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                }}  
+            >
+                {work.title}
+                <IconButton onClick={handleClose}>
+                    <CloseIcon/>
+                </IconButton>
+            </DialogTitle>
+            <SwipeableViews 
+                index={activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents
+            >
+                {work.subImgSrc.map((src, i) => {
+                    return (
+                        <div key={i}>
+                            <img 
+                                src={src} 
+                                style={{ width: '100%' }} 
+                            />
+                        </div>
+                    )
+                })}
+            </SwipeableViews>
+            <MobileStepper
+                steps={maxSteps}
+                position="static"
+                activeStep={activeStep}
+                sx={{ display: maxSteps > 1 ? 'flex' : 'none', justifyContent: 'center' }}
+            />
         </Dialog>
     )
 }
